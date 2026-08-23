00ad47a4e5b02fb431e783409486232052ec467e
Mani <manichinniah94@gmail.com>
2026-08-23 12:42:23 +0530

    enhanced sum function

--- 04-functions/Sum.java
+++ 04-functions/Sum.java
@@ -3,7 +3,19 @@ import java.util.Scanner;
 public class Sum {
     public static void main(String[] args){
         sum();
+        System.out.print("Sum = "+sum1());
     }
+
+    static int sum1(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        return sum;
+    }
+
     static void sum(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the first number: ");
@@ -11,6 +23,6 @@ public class Sum {
         System.out.print("Enter the second number: ");
         int num2 = sc.nextInt();
         int sum = num1 + num2;
-        System.out.print("Sum = "+sum);
+        System.out.println("Sum = "+sum);
     }
 }
